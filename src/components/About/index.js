165c34b50ed22517059ccbe541a3a5244bb32b1e
shane <shane.messmer1@gmail.com>
2021-11-01 14:19:10 -0400

    updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import ShanePic from '../../img/shane.png';
+
 
 const About=()=> {
   return(
     <div>
     <h2>About Me</h2>
     <div className="img">
-    <img src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/137587392_1783347638534591_5126202794657090851_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=7YIHy-YmJpoAX80HX3M&_nc_ht=scontent-lga3-1.xx&oh=043620161a4cd0fbb3d2f1a63ff709ba&oe=61656D04" alt="Shane" height="25%" width="25%" ></img>
+    <img src={ShanePic} alt="Shane" height="25%" width="25%" ></img>
     </div>
     <div className="aboutp">
       <p>Welcome to Shane's Brain, my fullstack engineering portfolio built using React. For the past two years I have 
         been learning fullstack software engineering with the goal of becoming a Jr. Software Developer. In my portfolio you'll find 
-        different projects using a multitude of languages. From a fullstack Blog application, to utilities for developers using Inquirer, 
-        you'll find I have acquired a plethora software engineering techniques. I am extremely excited
-        to become a part of the technological revolution we are experiencing. 
+        different projects using a multitude of languages. From a fullstack social media application, to utilities for developers using Inquirer, 
+        you'll find I have acquired many software engineering techniques. I am extremely excited
+        to become a part of the greatest technological revolution we are currently experiencing. 
       
 
       </p>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,7 +6,7 @@ function Resume() {
     <div>
       <h2>Resume</h2>
       <section className="resume">
-        <a href="https://docs.google.com/document/d/1jC-B4i9X7U9xbraoieQ988BFtunytmf4oyoVFmlhZGE/edit?usp=sharing"><img src={ResumePic} alt="resume" /></a>
+        <a href="https://drive.google.com/file/d/1rWEOocCZQxrzE4_0Vii_mhXaD_I_nwS6/view?usp=sharing"><img src={ResumePic} alt="resume" /></a>
       </section>
     </div>
 
